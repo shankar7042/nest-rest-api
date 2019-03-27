29ebcd547ebfe112f1e5f3cf12f1267133cf7654
shankar7042 <shankar.kumar.4171@gmail.com>
2019-03-27 23:53:39 +0530

    first commit

---
+++ src/config/keys.ts
@@ -0,0 +1,4 @@
+export default {
+  mongoURI:
+    'mongodb+srv://shankar4171:shankar4171@cluster0-r6new.mongodb.net/test?retryWrites=true',
+};
---
+++ src/items/items.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Put,
+} from '@nestjs/common';
+import { CreateItemDto } from './dto/create-item-dto';
+import { Item } from './interfaces/items.interface';
+import { ItemsService } from './items.service';
+
+@Controller('items')
+export class ItemsController {
+  constructor(private readonly itemService: ItemsService) {}
+
+  @Get()
+  findAll(): Promise<Item[]> {
+    return this.itemService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id): Promise<Item> {
+    return this.itemService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() createItemDto: CreateItemDto): Promise<Item> {
+    return this.itemService.create(createItemDto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id): Promise<Item> {
+    return this.itemService.delete(id);
+  }
+
+  @Put(':id')
+  update(@Body() updateItemDto: CreateItemDto, @Param('id') id): Promise<Item> {
+    return this.itemService.update(id, updateItemDto);
+  }
+}
